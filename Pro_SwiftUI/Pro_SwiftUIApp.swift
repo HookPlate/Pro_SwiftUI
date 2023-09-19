c7468ecb5f1d5de53cfcb4db6f55251f9f3864f1
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-09-19 20:33:08 +0100

    Initial Commit

---
+++ Pro_SwiftUI/Pro_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_SwiftUIApp.swift
+//  Pro_SwiftUI
+//
+//  Created by robin tetley on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pro_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
